95e5b80db3465bd620a0e81e9d58768e685cca3f
Kakistocratic <oemanuelsen@gmail.com>
2025-11-10 13:01:07 +0100

    Radix Mobile menu setup

---
+++ src/Header/MobileMenu.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import type { Header } from '@/payload-types'
+
+import { CMSLink } from '@/components/Link'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import { MenuIcon } from 'lucide-react'
+import { usePathname, useSearchParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+
+interface Props {
+  menu: Header['navItems']
+}
+
+export function MobileMenu({ menu }: Props) {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const closeMobileMenu = () => setIsOpen(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsOpen(false)
+      }
+    }
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [isOpen])
+
+  useEffect(() => {
+    setIsOpen(false)
+  }, [pathname, searchParams])
+
+  return (
+    <Sheet onOpenChange={setIsOpen} open={isOpen}>
+      <SheetTrigger className="relative flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:bg-black dark:text-white">
+        <MenuIcon className="h-4" />
+      </SheetTrigger>
+
+      <SheetContent side="left" className="px-4">
+        <SheetHeader className="px-0 pt-4 pb-0">
+          <SheetTitle>Menu</SheetTitle>
+          <SheetDescription />
+        </SheetHeader>
+
+        <div className="py-4">
+          {menu?.length ? (
+            <ul className="flex w-full flex-col">
+              {menu.map((item) => (
+                <li className="py-2" key={item.id}>
+                  <CMSLink {...item.link} appearance="link" />
+                </li>
+              ))}
+            </ul>
+          ) : null}
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
